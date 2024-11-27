48dbd2bac95ade0053e3bc8b3306e80aac2ee052
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-11-28 05:29:57 +0800

    Added IssueTypes and FeedbackTypes

--- utils/enums.js
+++ utils/enums.js
@@ -12,20 +12,44 @@ const PriorityTypes = Object.freeze({
 
 const StatusTypes = Object.freeze({
     Open: "OPEN",
+    Assigned: "ASSIGNED",
     InProgress: "IN_PROGRESS",
     Resolved: "RESOLVED",
     Invalid: "INVALID",
 });
 
+const IssueTypes = Object.freeze({
+    None: "NONE",
+    Bug: "BUG",
+    Feature: "FEATURE_REQUEST",
+    Help: "HELP",
+    Incident: "INCIDENT",
+    ServiceRequest: "SERVICE_REQUEST",
+    Support: "SUPPORT",
+});
+
+const FeedbackTypes = Object.freeze({
+    CustomerService: "CUSTOMER_SERVICE",
+    ProductReview: "PRODUCT_REVIEW",
+    ApplicationReview: "APPLICATION_REVIEW",
+    Others: "OTHERS",
+})
+
 const USER_TYPES = Object.values(UserTypes);
 const PRIORITY_TYPES = Object.values(PriorityTypes);
 const STATUS_TYPES = Object.values(StatusTypes);
+const ISSUE_TYPES = Object.values(IssueTypes);
+const FEEDBACK_TYPES = Object.values(FeedbackTypes);
 
 module.exports = {
     UserTypes,
     PriorityTypes,
     StatusTypes,
+    IssueTypes,
+    FeedbackTypes,
     USER_TYPES,
     PRIORITY_TYPES,
-    STATUS_TYPES
+    STATUS_TYPES,
+    ISSUE_TYPES,
+    FEEDBACK_TYPES,
 }
