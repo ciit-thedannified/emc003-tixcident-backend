605f9eb7a0b656f2467b9942f2a757b982901c13
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-11-28 16:49:10 +0800

    Changed schema data types on issue_id and author; added attachments field.

--- src/database/schemas/issue_messages_schema.js
+++ src/database/schemas/issue_messages_schema.js
@@ -2,19 +2,24 @@ const {model, Schema} = require('mongoose');
 
 const IssueMessagesSchema = new Schema({
     issue_id: {
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.String,
         ref: 'issues',
         required: true,
         immutable: true,
     },
     author: {
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.String,
+        ref: 'users',
         required: true,
         immutable: true,
     },
     message: {
         type: Schema.Types.String,
         required: true,
+    },
+    attachments: {
+        type: [Schema.Types.String],
+        default: [],
     }
 }, {
     timestamps: true,
