68aa75396605b8fe26dd876a19ed90fb962ab902
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-11-29 04:20:12 +0800

    Adjusted IssueMessagesSchema pre-hooks for find and findOne queries.

--- src/database/schemas/issue_messages_schema.js
+++ src/database/schemas/issue_messages_schema.js
@@ -1,5 +1,5 @@
 const {model, Schema} = require('mongoose');
-const {onIssueMessageCreated} = require("../middlewares/md_issue_messages_schema");
+const {onIssueMessageCreated, onIssueMessageFind} = require("../middlewares/md_issue_messages_schema");
 
 const IssueMessagesSchema = new Schema({
     issue_id: {
@@ -25,6 +25,10 @@ const IssueMessagesSchema = new Schema({
 }, {
     timestamps: true,
 });
+
+IssueMessagesSchema.pre('find', onIssueMessageFind);
+
+IssueMessagesSchema.pre('findOne', onIssueMessageFind);
 
 IssueMessagesSchema.pre('save', onIssueMessageCreated);
 
